a09b67b49cf8532285c49b94bf00a1dcd4defd22
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-10-22 21:16:52 -0700

    Make client get larger versions of images syncronously

--- src/Main.hs
+++ src/Main.hs
@@ -28,8 +28,6 @@ import qualified Data.ByteString.Char8 as NL8
 
 facebookPlatform = "facebook"
 
-data User = User { userId :: Int, userName :: String } deriving (Generic, Show)
-
 data Posts = Posts
     { posts :: [Post]
     , nexturl :: String
@@ -58,7 +56,7 @@ instance FromJSON FbPosts where
                 <*> v .: "paging"
     parseJSON _ = mzero
 
-fbFields = "link,message,description,caption,story,created_time,picture,from"
+fbFields = "link,message,description,caption,story,created_time,picture,from,object_id,type"
 
 data FbPost = FbPost
     { fbId :: String
@@ -69,6 +67,8 @@ data FbPost = FbPost
     , fbCaption :: Maybe String
     , fbFrom :: FbFrom
     , fbLink :: Maybe String
+    , fbObjectID :: Maybe String
+    , fbType :: String
     } deriving (Show, Generic, ToJSON)
 
 instance FromJSON FbPost where
@@ -81,6 +81,8 @@ instance FromJSON FbPost where
                <*> v .:? "caption"
                <*> v .: "from"
                <*> v .:? "link"
+               <*> v .:? "object_id"
+               <*> v .: "type"
     parseJSON _ = mzero
 
 data FbFrom = FbFrom
@@ -107,11 +109,15 @@ data FbError = FbError
     { error :: FbErrorContent
     } deriving (Show, Generic, ToJSON, FromJSON)
 
-bob :: User
-bob = User { userId = 1, userName = "bob" }
+data FbImages = FbImages
+    { images :: [FbImage]
+    } deriving (Show, Generic, ToJSON, FromJSON)
 
-jenny :: User
-jenny = User { userId = 2, userName = "jenny" }
+data FbImage = FbImage
+    { height :: Int
+    , source :: String
+    , width :: Int
+    } deriving (Show, Generic, ToJSON, FromJSON)
 
 routes :: ScottyM ()
 routes = do
@@ -121,23 +127,73 @@ routes = do
         postsRes <- liftIO(getPosts fbId fbToken)
         let res = fst postsRes
         let code = snd postsRes
+        let parsedPosts = (parsePosts res)
+        postsWithImages <- liftIO(updatePostsImages fbToken parsedPosts)
         status $ mkStatus code (NL8.pack "")
         if code == 200
-            then json $ makePostsGeneric $ parsePosts res
+            then json $ makePostsGeneric postsWithImages fbToken
             else json $ object [ "error" .= (getErrorMessage $ parseError res) ]
 
 getErrorMessage :: FbError -> String
 getErrorMessage FbError{Main.error=err} = message err
 
-makePostsGeneric :: FbPosts -> Posts
-makePostsGeneric posts =
-    Posts (Prelude.map makePostGeneric $ fbPosts posts) (Main.next $ paging posts)
+makePostsGeneric :: FbPosts -> String -> Posts
+makePostsGeneric posts fbToken =
+    Posts (Prelude.map (makePostGeneric fbToken) (fbPosts posts)) (Main.next $ paging posts)
 
 trimOffsetFromTime :: String -> String
 trimOffsetFromTime time = Prelude.take (Prelude.length time - 5) time
 
-makePostGeneric :: FbPost -> Post
-makePostGeneric fbPost = 
+-- TODO: Make this asyncronous
+updatePostsImages :: String -> FbPosts -> IO(FbPosts)
+updatePostsImages fbToken fbPostsWrap = do
+    let posts = fbPosts fbPostsWrap
+    updatedPosts <- mapM (updatePostImage fbToken) posts
+    return fbPostsWrap { fbPosts = updatedPosts }
+
+updatePostImage :: String -> FbPost -> IO (FbPost)
+updatePostImage fbToken fbPost = do
+    if (fbType fbPost) == "photo"
+        then do let id = (fromMaybe "" (fbObjectID fbPost))
+                photo <- getFbPhoto id fbToken
+                let updatedFbPost = fbPost { fbPicture = (Just photo) }
+                return updatedFbPost
+        else return fbPost
+
+getFbPhoto :: String -> String -> IO (String)
+getFbPhoto objectId fbToken = do
+    photos <- getPhotos objectId fbToken
+    let res = fst photos
+    let code = snd photos
+    if code == 200
+        then return $ fromMaybe "" (parseBestPhoto res 600)
+        else return ""
+
+parseBestPhoto :: String -> Int -> Maybe String
+parseBestPhoto photos targetWidth = 
+    let p = Data.Aeson.decode $ L8.pack photos :: Maybe FbImages
+    in  if isJust p
+            then getBestPhoto (fromJust p) targetWidth
+            else Nothing
+
+getBestPhoto :: FbImages -> Int -> Maybe String
+getBestPhoto photos targetWidth =
+    let options = images photos
+    in  if Prelude.length options == 0
+            then Nothing
+            else Just $ source $ Prelude.foldr (getBetterPhoto targetWidth) (Prelude.head options) options
+
+getBetterPhoto :: Int -> FbImage -> FbImage -> FbImage
+getBetterPhoto targetWidth first second
+    | w1 >= targetWidth && w2 < targetWidth = first
+    | w1 < targetWidth && w2 < targetWidth && w1 > w2 = first
+    | w1 >= targetWidth && w2 >= targetWidth && w1 < w2 = first
+    | otherwise = second
+    where w1 = width first
+          w2 = width second
+
+makePostGeneric :: String -> FbPost -> Post
+makePostGeneric fbToken fbPost = 
     Post { Main.id = fbId fbPost
          , profileimg = ""
          , date = (trimOffsetFromTime $ fbCreatedTime fbPost) ++ "Z"
@@ -161,6 +217,17 @@ parseError err =
     in  if isJust parsed
             then fromJust parsed
             else FbError $ FbErrorContent  ""
+
+getPhotos :: String -> String -> IO (String, Int)
+getPhotos objectId fbToken = do
+    initReq <- parseRequest ("https://graph.facebook.com/" ++ objectId ++ "?fields=images")
+    let req = initReq 
+                { requestHeaders =
+                    [ ("Authorization", NL8.pack ("Bearer " ++ fbToken)) ]
+                }
+    response <- httpLBS req
+    let code = getResponseStatusCode response
+    return (L8.unpack $ getResponseBody response, code)
 
 getPosts :: String -> String -> IO (String, Int)
 getPosts fbId fbToken = do
